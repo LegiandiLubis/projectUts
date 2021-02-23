0f43fe7409cb5a9b736ce88e14b902984084d0de
LegiandiLubis <79128625+LegiandiLubis@users.noreply.github.com>
2021-02-23 13:08:38 +0700

    Add files via upload

---
+++ ProjectMobileUTSActivity.java
@@ -0,0 +1,13 @@
+package com.widget;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProjectMobileUTSActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
